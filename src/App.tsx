b31b7ac6cba868781ba4cb0e8a5cd89ec6ba071d
TenViki <viktorkomarek28@gmail.com>
2022-03-18 08:05:13 +0100

    :sparkles: Adds header

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
-import { useState } from 'react'
+import Header from "./components/header/Header";
 
 function App() {
   return (
     <div className="app">
+      <Header />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/header/Header.tsx
@@ -0,0 +1,14 @@
+import "./header.scss";
+
+const Header = () => {
+  return (
+    <header className="container">
+      <nav>
+        <div className="nav-logo">NoteSpaces</div>
+        <div className="nav-theme">Theme</div>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
