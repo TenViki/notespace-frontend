9c2e10ef551d946ace7fcf8bf770b48478848931
TenViki <viktorkomarek28@gmail.com>
2022-03-18 13:07:39 +0100

    :sparkles: Adds add note button

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
+import { FiPlus } from "react-icons/fi";
 import Calendar from "./components/calendar/Calendar";
+import NewNote from "./components/forms/NewNote";
 import Header from "./components/header/Header";
 
 function App() {
   return (
     <div className="app">
-      <Header />
-      <Calendar />
+      <div className="main">
+        <Header />
+        <Calendar />
+
+        <button className="add-new">
+          <FiPlus />
+          <div className="text">Add note</div>
+        </button>
+      </div>
     </div>
   );
 }
---
+++ src/components/forms/NewNote.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { FiPlus } from "react-icons/fi";
+
+const NewNote = () => {
+  return <div></div>;
+};
+
+export default NewNote;
