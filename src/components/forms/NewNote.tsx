f812a1c6fabc267d77e97672ffcadfa6d32a9a57
TenViki <viktorkomarek28@gmail.com>
2022-03-18 14:52:22 +0100

    :sparkles: Adds styling for input

--- src/components/forms/NewNote.tsx
+++ src/components/forms/NewNote.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { FiPlus } from "react-icons/fi";
+import "./newnote.scss";
 import SlideMenu from "../shared/SlideMenu";
 
 interface NewNoteProps {
@@ -9,12 +9,21 @@ interface NewNoteProps {
 
 const NewNote: React.FC<NewNoteProps> = ({ opened, close }) => {
   return (
-    <SlideMenu
-      opened={opened}
-      close={close}
-      title="New note"
-      width="20rem"
-    ></SlideMenu>
+    <>
+      <SlideMenu opened={opened} close={close} title="New note" width="20rem">
+        <input type="text" placeholder="Tag" />
+        <input type="text" placeholder="Label" />
+        <input type="date" placeholder="For date" />
+
+        <div className="add-note-files">
+          <div className="file-title">Attachements:</div>
+        </div>
+
+        <button>
+          <div className="text">Create</div>
+        </button>
+      </SlideMenu>
+    </>
   );
 };
 
